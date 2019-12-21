8af6909aac3c80da6de2d0f7216275d4692437cb
LuciferLX <173355796@qq.com>
2019-12-21 23:28:25 +0800

    将Lombok集成到IDEA中并将用户头像存入数据库

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -53,6 +53,7 @@ public class AuthorizeController {
             user.setAccount_id(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.insert(user);
             response.addCookie(new Cookie("token",token));
             //request.getSession().setAttribute("user",githubUser);
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -7,7 +7,7 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{account_id},#{token},#{gmtCreate},#{gmtModified})")
+    @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified,avatar_url) values (#{name},#{account_id},#{token},#{gmtCreate},#{gmtModified},#{avatarUrl})")
     void insert(User user);
     @Select("select * from user where token = #{token}")
     User findByToken(String token);
