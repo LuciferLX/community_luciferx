de8eb59d4cc5e1162e40a5f665a6f64a64865654
LuciferLX <173355796@qq.com>
2019-12-26 22:34:40 +0800

    添加了问题修改功能

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -88,4 +88,15 @@ public class QuestionService {
         questionDTO.setUser(user);
         return questionDTO;
     }
+
+    public void createOrUpdate(Question question) {
+        if(question.getId()>0){
+            question.setGmtModified(question.getGmtCreate());
+            questionMapper.update(question);
+        }else{
+            question.setGmtCreate(System.currentTimeMillis());
+            question.setGmtModified(question.getGmtCreate());
+            questionMapper.create(question);
+        }
+    }
 }
